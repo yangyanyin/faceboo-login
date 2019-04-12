ee69be710cf4d91ff77c52bd732ef585975357b3
yangyanyin <yanyin.yang@patpat.com>
2019-04-12 11:37:37 +0800

    y

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,67 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="facebook-login">
+    <h3>Facebook 登陆</h3>
+    <a href="javascript:void(0);" @click.stop.prevent="facebookLogin">Sign In with Facebook`</a>
   </div>
 </template>
-
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  created () {
+    (function (d, s, id) {
+      // eslint-disable-next-line
+      let js, fjs = d.getElementsByTagName(s)[0]
+      if (d.getElementById(id)) {
+        return
+      }
+      js = d.createElement(s)
+      js.id = id
+      js.src = 'https://connect.facebook.net/en_US/sdk.js'
+      fjs.parentNode.insertBefore(js, fjs)
+    }(document, 'script', 'facebook-jssdk'))
+
+    // eslint-disable-next-line
+    window.fbAsyncInit = function () {
+      // eslint-disable-next-line
+      FB.init({
+        appId: 270202583150184,
+        cookie: true,
+        xfbml: true,
+        version: 'v3.2' // use graph api version 3.2,
+      })
+    }
+  },
+  methods: {
+    facebookLogin () {
+      let _this = this
+      // eslint-disable-next-line
+      FB.getLoginStatus(function (response) {
+        if (response.status === 'connected') {
+          let userId = response.authResponse.userID
+          _this.getFaceBookUserInfo(userId)
+        } else {
+          // eslint-disable-next-line
+          FB.login(_this.checkLogin, { scope: 'email,public_profile', return_scopes: true })
+        }
+      })
+    },
+    checkLogin (response) {
+      if (response.status === 'connected') {
+        let userId = response.authResponse.userID
+        this.getFaceBookUserInfo(userId)
+      }
+    },
+    getFaceBookUserInfo (userId) {
+      let _this = this
+      // eslint-disable-next-line
+      // fields=id,first_name,last_name,email 获取facebook对应的用户信息
+      FB.api( '/' + userId + '/?fields=id,first_name,last_name,email', 'GET',{}, function (response) {
+        _this.facebookSuccessLogin(response)
+      })
+    },
+    //  facebook登陆成功后
+    facebookSuccessLogin (response) {
+      console.log(response)
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
